fcdaf26f703672475c18a853881e2dc6a87e74c3
mark <github@alternity.org.uk>
2013-03-13 00:29:48 +0000

    better land masses (quite a lot of aliasing)

--- theia_test/shaders/test.fs.glsl
+++ theia_test/shaders/test.fs.glsl
@@ -31,9 +31,22 @@ float fBm(vec2 P, float octaves, float lacunarity, float gain)
   }
   return sum;
 }
+float fBm(vec3 P, float octaves, float lacunarity, float gain)
+{
+  float frequency = 1;
+  float amplitude = 0.5;
+  float sum = 0;
+  for (int i = 0; i < octaves; i++)
+  {
+    sum += snoise(P * frequency) * amplitude;
+    frequency *= lacunarity;
+    amplitude *= gain;
+  }
+  return sum;
+}
 
 // Similar to fBm but uses the sum of abs(noise).
-float Turbulence(vec2 P, int octaves, float lacunarity, float gain)
+float Turbulence(vec2 P, float octaves, float lacunarity, float gain)
 {
   float frequency = 1;
   float amplitude = 0.5;
@@ -54,20 +67,50 @@ vec2 VectorNoise(vec2 P) { return vec2(snoise(P), snoise(P + 3.33)); }
 
 float DistNoise(vec2 P, float distortion) { return snoise(P + (distortion * VectorNoise(P + 0.5))); }
 
+#define VERY_SMALL 0.0001
 #define MINFILTWIDTH 1.0e-6
 #define MINFILTWIDTH_SQUARED  (MINFILTWIDTH * MINFILTWIDTH)
-#define FILTERWIDTH(x)  max( LengthSquared(abs(dFdx(x)) + abs(dFdy(x))), MINFILTWIDTH_SQUARED)
+#define FILTERWIDTH(x)  max( LengthSquared(fwidth(x)), MINFILTWIDTH_SQUARED)
 
-bool IsLand(vec2 P)
+#define FilteredSNoise(p,width)           (snoise(p) * (1 - smoothstep (0.2,0.6,width)))
+#define FilteredSNoiseV(p,width)          (VectorNoise(p) * (1-smoothstep (0.2,0.6,width)))
+#define FilteredDistNoise(Pt,width,scale) (FilteredSNoise(FilteredSNoiseV(Pt,width)*scale+Pt,width))
+
+bool IsLand(vec3 P)
 {
-  const float lacunarity = 2.1759;
+  const float lacunarity = 3.5;
   const float octaves = 7;
-  const float gain = 0.5;
+  const float gain = 0.5123;
   const float seaLevel = 0;
   const float mountainScale = 1;
   const float bumpScale = 0.07;
-  float bumpy = fBm(P, octaves, lacunarity, gain);
-  float chaos = bumpy * bumpScale;
+
+  float bumpy;
+
+  // "Simple" fBm bump:
+  bumpy = fBm(P, octaves, lacunarity, gain);
+
+  // Multifractal bump:
+  //{
+  //  const float distortion = 0.2;
+  //  float spectralExponent = 0.5;
+
+  //  float filterWidth = FILTERWIDTH(P);
+  //  vec2 P2 = P + distortion * FilteredSNoiseV(P, filterWidth);
+  //  float fw = filterWidth;
+  //  float weight = abs(FilteredDistNoise(P2, fw, 1.5));
+  //  bumpy = weight * FilteredSNoise(P2, fw);
+  //  for (float i = 0; (i < octaves) && (weight >= VERY_SMALL) && (fw < 1); ++i)
+  //  {
+  //    P2 *= lacunarity;
+  //    fw *= filterWidth;
+  //    weight *= (i * spectralExponent) * (0.7 + snoise(P2));
+  //    weight = clamp(abs(weight),0,1);
+  //    bumpy += snoise(P2) * min(weight, spectralExponent);
+  //  }
+  //}
+
+  float chaos = bumpy;
   return (chaos > seaLevel);
 }
 
@@ -111,14 +154,15 @@ void main()
 	// with the expensive lighting calculation if it is...
 	//if (OnLatLonLine(textureCoord))
 	//{
-	//	fragColour = vec4(1,0,0,1);
+	//	fragColour = vec4(0.05,0.05,0.05,1);
 	//}
 	//else
 	{
 		MaterialStruct material = Material;
 
 		//material.Kd = vec3(Turbulence(textureCoord, 16, 5.1753, 0.49357));
-    material.Kd = mix(sea, land, IsLand(textureCoord));
+    //material.Kd = mix(sea, land, IsLand(textureCoord));
+    material.Kd = mix(sea, land, IsLand(objectNormal));
 
 		vec3 N = normalize(vertexSurfaceNormal);
 		fragColour.rgb = ComputeLight(vertexWorldPos, N, material);
